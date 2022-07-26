b2d571350d194c51a4c3665d6bdd3cd241b6e1d8
ChristianRV29 <kmylorv@gmail.com>
2022-07-25 22:06:02 -0500

    Finish to move styles

--- src/screens/PermissionScreen.tsx
+++ src/screens/PermissionScreen.tsx
@@ -1,25 +1,30 @@
 import React, { useContext } from 'react';
 
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { PermissionsContext } from '~src/context/PermissionsContext';
+import { permissionsScreenStyles } from '~src/styles';
 
 export const PermissionScreen = () => {
   const { permissions, askLocationPermission } = useContext(PermissionsContext);
 
   return (
-    <View style={styles.mainWrapper}>
-      <View style={styles.contentWrapper}>
-        <Text style={styles.text}>Permissions screen</Text>
+    <View style={permissionsScreenStyles.mainWrapper}>
+      <View style={permissionsScreenStyles.contentWrapper}>
+        <Text style={permissionsScreenStyles.text}>Permissions screen</Text>
       </View>
       <TouchableOpacity
         onPress={() => askLocationPermission()}
-        style={styles.button}>
-        <Text style={styles.textButton}>Check permissions</Text>
+        style={permissionsScreenStyles.button}>
+        <Text style={permissionsScreenStyles.textButton}>
+          Check permissions
+        </Text>
         <Icon name="lock-closed-outline" size={20} color={'white'} />
       </TouchableOpacity>
-      <Text style={styles.permissionsText}>{JSON.stringify(permissions)}</Text>
+      <Text style={permissionsScreenStyles.permissionsText}>
+        {JSON.stringify(permissions)}
+      </Text>
     </View>
   );
 };
