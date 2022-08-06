6c502479652e7401c99fa91281e32849b08c4c1c
ChristianRV29 <kmylorv@gmail.com>
2022-08-05 20:18:34 -0500

    Configure React-native-maps for Android

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { View, StyleSheet } from 'react-native';
+// import Icon from 'react-native-vector-icons/Ionicons';
+
+import MapView from 'react-native-maps';
 
 import { mapScreenStyles } from '~src/styles';
 
 export const MapScreen = () => {
   return (
     <View style={mapScreenStyles.mainWrapper}>
-      <Text style={mapScreenStyles.text}>Welcome to Routes app</Text>
-      <Icon name="map-outline" size={30} color={'black'} />
+      <MapView
+        style={{ ...StyleSheet.absoluteFillObject }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+      {/* <Text style={mapScreenStyles.text}>Welcome to Routes app</Text>
+      <Icon name="map-outline" size={30} color={'black'} /> */}
     </View>
   );
 };
