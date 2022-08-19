667752b8efc8b00f894a138f939c8a269ae60544
ChristianRV29 <kmylorv@gmail.com>
2022-08-19 09:50:12 -0500

    Create an icon for centring screen

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,11 @@
 import React, { useId } from 'react';
 import { StyleSheet } from 'react-native';
-
 import MapView, { MapMarkerProps, Marker } from 'react-native-maps';
+
 import { useLocation } from '~src/hooks/useLocation';
 import { LoadingScreen } from '~src/screens/LoadingScreen';
+import { FabIcon } from '~src/components/FabIcon/FabIcon';
+import { fabIconStyles } from '../../styles/index';
 
 interface Props {
   markers?: MapMarkerProps[];
@@ -19,18 +21,26 @@ export const Map: React.FC<Props> = ({ markers }) => {
   }
 
   return (
-    <MapView
-      initialRegion={{
-        latitude: userLocation.latitude,
-        longitude: userLocation.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      showsUserLocation
-      style={{ ...StyleSheet.absoluteFillObject }}>
-      {markers?.map((markerData, index) => (
-        <Marker key={`${index} - ${id}`} {...markerData} />
-      ))}
-    </MapView>
+    <>
+      <MapView
+        initialRegion={{
+          latitude: userLocation.latitude,
+          longitude: userLocation.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation
+        style={{ ...StyleSheet.absoluteFillObject }}>
+        {markers?.map((markerData, index) => (
+          <Marker key={`${index} - ${id}`} {...markerData} />
+        ))}
+      </MapView>
+      <FabIcon
+        iconName="compass-outline"
+        iconSize={50}
+        styles={fabIconStyles.mainWrapper}
+        onPress={() => console.log('Centring')}
+      />
+    </>
   );
 };
