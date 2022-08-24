ba2bcd38e8fd59743b4cdeb08bad434cf9037ab1
ChristianRV29 <kmylorv@gmail.com>
2022-08-24 10:04:50 -0500

    Remove warning message from metro console

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { enableLatestRenderer } from 'react-native-maps';
 
 import { StackNavigator } from '~src/navigation/StackNavigator';
 import { PermissionsProvider } from '~src/context/PermissionsContext';
+
+LogBox.ignoreLogs(['NativeEventEmitter']);
 
 enableLatestRenderer();
 
